53ffd8659573e9d0771bb9a29a18aa3ffe6662f5
zhongpei <zhongpei@vip.qq.com>
2017-12-27 19:32:28 +0800

    ifix

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y -q gcc make wget python python-pip pppoe git python-dev
+sudo apt-get install -y -q net-tools pppoeconf psmisc
+sudo apt-get install -y -q rsyslog
+
 pip install pyinstaller -i http://pypi.douban.com/simple/
 pyinstaller -F ./endpoints/hkpon.py
 pyinstaller -F ./endpoints/hkpoff.py
--- package.sh
+++ package.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 rm -fr ./hkppp
+mkdir -p hkppp/etc/ppp/peers
 mkdir -p hkppp/usr/local/sbin
 mkdir -p hkppp/DEBIAN
+cp ./pap-secrets.tp hkppp/etc/ppp/peers
 cp ./control hkppp/DEBIAN
 cp ./dist/* hkppp/usr/local/sbin/
 dpkg-deb --build hkppp
