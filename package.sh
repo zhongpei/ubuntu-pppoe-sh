11f7c03a6ba221d7e5193a05dff2e0e3bde1e61f
zhongpei <zhongpei@vip.qq.com>
2017-12-28 15:41:16 +0800

    fix

--- package.sh
+++ package.sh
@@ -4,7 +4,8 @@ rm -fr ./hkppp
 mkdir -p hkppp/etc/ppp/peers
 mkdir -p hkppp/usr/local/sbin
 mkdir -p hkppp/DEBIAN
-cp ./pap-secrets.tp hkppp/etc/ppp/peers
+cp ./pap-secrets.tp hkppp/etc/ppp
+cp ./dsl-provider.tp hkppp/etc/ppp/peers
 cp ./control hkppp/DEBIAN
 cp ./dist/* hkppp/usr/local/sbin/
 dpkg-deb --build hkppp
